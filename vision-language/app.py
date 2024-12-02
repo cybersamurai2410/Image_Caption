206dbb05c310a7fe01997aeb37b988e56ca92666
Aditya Satheesh <66138996+cybersamurai2410@users.noreply.github.com>
2024-12-02 19:49:00 +0530

    Update app.py

--- vision-language/app.py
+++ vision-language/app.py
@@ -1,39 +1,7 @@
 import gradio as gr
-from transformers import AutoProcessor, BlipForConditionalGeneration, BlipForImageTextRetrieval, BlipForQuestionAnswering
-from PIL import Image
-import torch
-import requests
-
-# Load models and processors
-caption_model = BlipForConditionalGeneration.from_pretrained("./models/Salesforce/blip-image-captioning-base")
-caption_processor = AutoProcessor.from_pretrained("./models/Salesforce/blip-image-captioning-base")
-
-retrieval_model = BlipForImageTextRetrieval.from_pretrained("./models/Salesforce/blip-itm-base-coco")
-retrieval_processor = AutoProcessor.from_pretrained("./models/Salesforce/blip-itm-base-coco")
-
-vqa_model = BlipForQuestionAnswering.from_pretrained("./models/Salesforce/blip-vqa-base")
-vqa_processor = AutoProcessor.from_pretrained("./models/Salesforce/blip-vqa-base")
-
-# Image Captioning
-def image_captioning(image):
-    inputs = caption_processor(image, "a photograph of", return_tensors="pt")
-    out = caption_model.generate(**inputs)
-    return caption_processor.decode(out[0], skip_special_tokens=True)
-
-# Image Retrieval
-def image_retrieval(image_url, text):
-    raw_image = Image.open(requests.get(image_url, stream=True).raw).convert('RGB')
-    inputs = retrieval_processor(images=raw_image, text=text, return_tensors="pt")
-    itm_scores = retrieval_model(**inputs)[0]
-    itm_score = torch.nn.functional.softmax(itm_scores, dim=1)
-    probability = itm_score[0][1].item()
-    return f"The image and text are matched with a probability of {probability:.4f}"
-
-# Visual Question Answering
-def visual_qa(image, question):
-    inputs = vqa_processor(image, question, return_tensors="pt")
-    out = vqa_model.generate(**inputs)
-    return vqa_processor.decode(out[0], skip_special_tokens=True)
+from tasks.image_caption import image_captioning
+from tasks.image_retrieval import image_retrieval
+from tasks.visual_qa import visual_qa
 
 # Gradio interfaces
 caption_interface = gr.Interface(
