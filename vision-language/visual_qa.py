5cd43239d33b747de9aa604905297167329f436c
Aditya Satheesh <66138996+cybersamurai2410@users.noreply.github.com>
2024-12-03 17:30:43 +0530

    Update visual_qa.py

--- vision-language/visual_qa.py
+++ vision-language/visual_qa.py
@@ -1,11 +1,10 @@
 from transformers import AutoProcessor, BlipForQuestionAnswering
-from PIL import Image
 
 vqa_id = "Salesforce/blip-vqa-base"
 vqa_model = BlipForQuestionAnswering.from_pretrained(vqa_id)
 vqa_processor = AutoProcessor.from_pretrained(vqa_id)
 
 def visual_qa(image, question):
-    inputs = processor(image, question, return_tensors="pt")
-    out = model.generate(**inputs)
-    return processor.decode(out[0], skip_special_tokens=True)
+    inputs = vqa_processor(image, question, return_tensors="pt")
+    out = vqa_model.generate(**inputs)
+    return vqa_processor.decode(out[0], skip_special_tokens=True)
